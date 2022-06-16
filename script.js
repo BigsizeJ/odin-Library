e73a76ac36cc2581b2e29d76f98d6f48cdb07fc4
BigsizeJ <apacjessie@gmail.com>
2022-06-16 22:30:46 +0800

    Update add animation

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const add = document.querySelector('#add')
 const modal = document.querySelector('.modal')
-
+const theme = document.querySelector('#theme')
+const header = document.querySelector('.header')
+const container = document.querySelector('.container')
+const add_icon = document.querySelector('.add-img')
 
 add.addEventListener('click', () =>{
-    modal.style.display = "block";
-    console.log("hello")
+    modal.style.display = 'block'
+    modal.classList.toggle('pop')
+    add_icon.classList.toggle('close-img')
+})
+
+
+theme.addEventListener('click', () => {
+    container.classList.toggle('dark')
+    header.classList.toggle('darkHeader')
 })
